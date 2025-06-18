39731c03bdf33e42cb9a9771f2ecce941d7b6dfb
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-06-18 23:50:38 +0100

    Add z and vim colors

--- src/config.zsh
+++ src/config.zsh
@@ -2,7 +2,7 @@
 # ~~~~~~~~~~~~~~~~~~~
 
 plugin_source_git() {
-	ZSH_PLUGIN="$HOME/.zsh_plugins/$2/$2.zsh"
+	ZSH_PLUGIN="$HOME/.zsh_plugins/$2/$3"
 	if [ ! -f "$ZSH_PLUGIN" ]; then
 		git clone "git@github.com:$1/$2.git" "$HOME/.zsh_plugins/$2"
 	fi
@@ -10,14 +10,14 @@ plugin_source_git() {
 }
 
 # Plugins
-plugin_source_git "zsh-users" "zsh-history-substring-search"
+plugin_source_git "zsh-users" "zsh-history-substring-search" "zsh-history-substring-search.zsh"
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
-plugin_source_git "zsh-users" "zsh-autosuggestions"
-plugin_source_git "zsh-users" "zsh-syntax-highlighting"
-
+plugin_source_git "zsh-users" "zsh-autosuggestions" "zsh-autosuggestions.zsh"
+plugin_source_git "zsh-users" "zsh-syntax-highlighting" "zsh-syntax-highlighting.zsh"
+plugin_source_git "agkozak" "zsh-z" "zsh-z.plugin.zsh"
 
 # ~~~~~~~~~~~~~~~~~~~
 # Vars
@@ -58,7 +58,7 @@ if [ ! $commands[apt] ]; then
 	alias up="_ apt update;_ apt -y full-upgrade;_ apt -y autoremove"
 fi
 
-alias install-vim="wget -O ~/.vimrc https://raw.githubusercontent.com/mathiasbynens/dotfiles/main/.vimrc ; mkdir -p ~/.vim/swaps ~/.vim/backups ~/.vim/undo"
+alias install-vim="wget -O ~/.vimrc https://raw.githubusercontent.com/mathiasbynens/dotfiles/main/.vimrc ; mkdir -p ~/.vim/swaps ~/.vim/backups ~/.vim/undo ~/.vim/colors; wget -O ~/.vim/colors/solarized.vim https://raw.githubusercontent.com/mathiasbynens/dotfiles/main/.vim/colors/solarized.vim"
 
 if [ ! $commands[starship] ]; then
 	set -x
